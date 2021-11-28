82c8066583d7f48e88dee18b18902ad372d27fde
Serkan ÖZAL <serkan@thundra.io>
2021-11-28 09:31:57 +0300

    Initial impl (#1)

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# middy-context
+# middy-invocation
+
+Middy middleware for accessing current AWS Lambda invocation event and context from anywhere 
+without need to passing event and context as arguments through your code.
+
+
+## Installation
+
+To install the middleware, you can use NPM:
+
+```
+npm install --save middy-invocation
+```
+
+**Note:** Requires `@middy/core` version `2.0.0`+
+
+
+## Usage
+
+- Register `middy-invocation` middleware in your handler:
+```javascript
+const middy = require('@middy/core');
+const invocation = require('middy-invocation');
+
+const handler = async(event, context) => {
+  // Do something meaningful
+
+  return {
+    statusCode: 200,
+  }
+}
+
+module.exports.handler = middy(handler).use(invocation());
+```
+
+- Access event and context from anywhere/module/file:
+```javascript
+const { event, context } = require('middy-invocation');
+
+const evt = event();
+const ctx = context();
+const awsRequestId = ctx.awsRequestId; // or context().awsRequestId;
+const remainingTimeInMillis = ctx.getRemainingTimeInMillis(); // or context().getRemainingTimeInMillis();
+```
+
+
+## Contributing
+
+Everyone is very welcome to contribute to this repository. 
+Feel free to [raise issues](https://github.com/serkan-ozal/middy-invocation/issues) 
+or to [submit Pull Requests](https://github.com/serkan-ozal/middy-invocation/pulls).
+
+
+## License
+
+Licensed under [MIT License](LICENSE).
---
+++ src/index.js
@@ -0,0 +1,50 @@
+let evt, ctx;
+
+const event = () => {
+    return evt;
+}
+
+const context = () => {
+    return ctx;
+}
+
+const invocationMiddleware = (opts = {}) => {
+
+    const invocationMiddlewareBefore = async (request) => {
+        evt = request.event;
+        ctx = {
+            functionVersion: request.context.functionVersion,
+            functionName: request.context.functionName,
+            memoryLimitInMB: request.context.memoryLimitInMB,
+            logGroupName: request.context.logGroupName,
+            logStreamName: request.context.logStreamName,
+            clientContext: request.context.clientContext,
+            identity: request.context.identity,
+            invokedFunctionArn: request.context.invokedFunctionArn,
+            awsRequestId: request.context.awsRequestId,
+            getRemainingTimeInMillis: request.context.getRemainingTimeInMillis,
+        }
+    }
+
+    const invocationMiddlewareAfter = async (request) => {
+        evt = null;
+        ctx = null;
+    }
+
+    const invocationMiddlewareOnError = async (request) => {
+        evt = null;
+        ctx = null;
+    }
+
+    return {
+        before: invocationMiddlewareBefore,
+        after: invocationMiddlewareAfter,
+        onError: invocationMiddlewareOnError,
+    }
+
+}
+
+module.exports = invocationMiddleware;
+module.exports.mw = invocationMiddleware;
+module.exports.event = event;
+module.exports.context = context;
